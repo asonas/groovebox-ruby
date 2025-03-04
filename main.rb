3b800ac0f282395b4e351f0867115064886251be
Yuya Fujiwara <hzw1258@gmail.com>
2025-03-05 00:44:58 +0900

    Add DrumRack

--- main.rb
+++ main.rb
@@ -122,6 +122,61 @@ def handle_midi_signals(synthesizer, sequencer_player, config)
   end
 end
 
+# 規定のmidi_noteから4x4のドラムパッドを生成する
+# basa_midi_noteが68で4x8のデバイスの場合は以下のようにアサインされる
+# 92 93 94 95
+# 84 85 86 87
+# 76 77 78 79
+# 68 69 70 71
+class DrumRack
+  MAX_TRACKS = 16
+
+  def initialize(base_midi_node)
+    @tracks = {}
+    @base_midi_node = base_midi_node
+  end
+
+  def add_track(synth, pad_offset = 0)
+    midi_note = @base_midi_node + pad_offset
+    @tracks[midi_note] = synth
+  end
+
+  def note_on(midi_note, velocity)
+    if @tracks[midi_note]
+      @tracks[midi_note].note_on(midi_note, velocity)
+      puts "Note On: #{midi_note}, velocity=#{velocity}"
+    end
+  end
+
+  def note_off(midi_note)
+    if @tracks[midi_note]
+      @tracks[midi_note].note_off(midi_note)
+      puts "Note Off: #{midi_note}"
+    end
+  end
+
+  def generate(buffer_size)
+    mixed_samples = Array.new(buffer_size, 0.0)
+    active_tracks = 0
+
+    @tracks.each do |midi_note, synth|
+      samples = synth.generate(buffer_size)
+      next if samples.all? { |sample| sample.zero? }
+
+      mixed_samples = mixed_samples.zip(samples).map { |a, b| a + b }
+      active_tracks += 1
+    end
+    gain_adjustment = active_tracks > 0 ? 1.0 / active_tracks : 1.0
+    mixed_samples.map! { |sample| sample * gain_adjustment }
+
+    mixed_samples
+  end
+
+  def pad_notes
+    @tracks.keys
+  end
+end
+
 class SequencerPlayer
   def initialize(synthesizer, sequencer)
     @synthesizer = synthesizer
