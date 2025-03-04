c9952dee5662a2b664f34caf8e81a6cf3e6a2d8c
Yuya Fujiwara <hzw1258@gmail.com>
2025-03-05 00:45:56 +0900

    Implements Groovebox class

--- main.rb
+++ main.rb
@@ -13,21 +13,33 @@ require_relative "lib/synthesizer"
 require_relative "lib/note"
 require_relative "lib/vca"
 require_relative "lib/step"
+require_relative "lib/presets/kick"
 
-def handle_midi_signals(synthesizer, sequencer_player, config)
+def handle_midi_signals(groovebox, sequencer_player, config)
   midi_input = UniMIDI::Input.use(config['midi_device']['index'])
   midi_output = UniMIDI::Output.use(config['midi_device']['index'])
   puts "Listening for MIDI signals from #{midi_input.name}..."
 
   white_keys = [0, 2, 4, 5, 7, 9, 11] # C, D, E, F, G, A, B
+  drum_pad_range = (68..95).to_a
 
   loop do
-    # 白鍵のパッドを光らせる
-    (config['keyboard']['note_range']['start']..config['keyboard']['note_range']['end']).each do |midi_note|
-      if white_keys.include?(midi_note % 12)
-        midi_output.puts(0x90, midi_note, 120)
-      else
-        midi_output.puts(0x80, midi_note, 0)
+    if groovebox.current_instrument.is_a?(DrumRack)
+      drum_pad_range.each do |midi_note|
+        if groovebox.current_instrument.pad_notes.include?(midi_note)
+          midi_output.puts(0x90, midi_note, 120)
+        else
+          midi_output.puts(0x80, midi_note, 0)
+        end
+      end
+    elsif groovebox.current_instrument.is_a?(Synthesizer)
+      # 白鍵のパッドを光らせる
+      (config['keyboard']['note_range']['start']..config['keyboard']['note_range']['end']).each do |midi_note|
+        if white_keys.include?(midi_note % 12)
+          midi_output.puts(0x90, midi_note, 120)
+        else
+          midi_output.puts(0x80, midi_note, 0)
+        end
       end
     end
 
@@ -36,78 +48,66 @@ def handle_midi_signals(synthesizer, sequencer_player, config)
       # TODO: 254を受信出来なくなったらエラーを出して終了させる
       next if data[0] == 254 # Active Sensing を無視
 
-      case data[0] & 0xF0
-      when 0x90 # 144: Note On
-        midi_note = data[1]
-        velocity = data[2]
-        if velocity > 0
-          if (config['keyboard']['note_range']['start']..config['keyboard']['note_range']['end']).include?(midi_note)
-            frequency = 440.0 * (2 ** ((midi_note - 69) / 12.0))
-            synthesizer.note_on(midi_note, frequency)
-            puts "Note On: #{Note::NOTE_NAMES[(midi_note % 12)]} (#{frequency.round(2)} Hz)"
-          elsif config['switches'].key?(midi_note.to_s)
-            synthesizer.oscillator.waveform = config['switches'][midi_note.to_s].to_sym
-            puts "Waveform changed to: #{synthesizer.oscillator.waveform.capitalize}"
-          end
-        end
-      when 0x80 # 128: Note Off or Note On with velocity 0
-        midi_note = data[1]
-        if (config['keyboard']['note_range']['start']..config['keyboard']['note_range']['end']).include?(midi_note)
-          synthesizer.note_off(midi_note)
-          puts "Note Off: #{Note::NOTE_NAMES[(midi_note % 12)]}"
-        end
-      when 0xB0 # 176: Control Change
-        control = data[1]
-        value = data[2]
+      status_byte = data[0]
+      midi_note = data[1]
+      velocity = data[2]
+
+      case status_byte & 0xF0
+      when 0x90 # Note On (channel 0 => 0x90, channel 1 => 0x91, etc.)
+        groovebox.current_instrument.note_on(midi_note, velocity)
+        puts "Note On: ch=#{status_byte & 0x0F}, midi_note=#{midi_note}, velocity=#{velocity}"
+
+      when 0x80 # Note Off
+        groovebox.current_instrument.note_off(midi_note)
+        puts "Note Off: ch=#{status_byte & 0x0F}, midi_note=#{midi_note}"
+      when 0xB0 # Control Change (channel 0=>0xB0,1=>0xB1, etc.)
+        control = midi_note
+        value = velocity
 
         # LPF / HPF
-        cutoff_change = value == 127 ? 10 : -10
-        if control == 71
-          synthesizer.vcf.low_pass_cutoff += cutoff_change
-          puts "VCF Low Pass Cutoff: #{synthesizer.vcf.low_pass_cutoff.round(2)} Hz"
-        elsif control == 72
-          synthesizer.vcf.high_pass_cutoff += cutoff_change
-          puts "VCF High Pass Cutoff: #{synthesizer.vcf.high_pass_cutoff.round(2)} Hz"
-        end
+        # cutoff_change = value == 127 ? 10 : -10
+        # if control == 71
+        #   synthesizer.vcf.low_pass_cutoff += cutoff_change
+        #   puts "VCF Low Pass Cutoff: #{synthesizer.vcf.low_pass_cutoff.round(2)} Hz"
+        # elsif control == 72
+        #   synthesizer.vcf.high_pass_cutoff += cutoff_change
+        #   puts "VCF High Pass Cutoff: #{synthesizer.vcf.high_pass_cutoff.round(2)} Hz"
+        # end
 
         # ADSR
-        # TODO: ADSRそれぞれの値を設定できる閾値を定義してその範囲で値を動かせるようにする
+        # Attack: 0.00~2.00
+        # Decay: 0.00~5.00
+        # Sustain: 0.00~1.00
+        # Release: 0.00~10.00
         case control
         when 73 # Attack
-          if value == 127
-            synthesizer.envelope.attack += 1
-          elsif value == 1
-            synthesizer.envelope.attack -= 1
-          end
-          synthesizer.envelope.attack = synthesizer.envelope.attack.clamp(0.01, 10.0)
-          puts "Attack: #{synthesizer.envelope.attack.round(2)}"
-
+          new_value = value == 127 ? -0.01 : 0.01
+          groovebox.current_instrument.envelope.attack =
+            (groovebox.current_instrument.envelope.attack + new_value).clamp(0.00, 2.00)
+          puts "Attack: #{groovebox.current_instrument.envelope.attack.round(2)}"
         when 74 # Decay
-          if value == 127
-            synthesizer.envelope.decay += 1
-          elsif value == 1
-            synthesizer.envelope.decay -= 1
-          end
-          synthesizer.envelope.decay = synthesizer.envelope.decay.clamp(0.01, 10.0)
-          puts "Decay: #{synthesizer.envelope.decay.round(2)}"
-
+          new_value = value == 127 ? 0.01 : -0.01
+          groovebox.current_instrument.envelope.decay = groovebox.current_instrument.envelope.decay.clamp(0.00, 5.00)
+          puts "Decay: #{groovebox.current_instrument.envelope.decay.round(2)}"
         when 75 # Sustain
-          if value == 127
-            synthesizer.envelope.sustain += 0.1
-          elsif value == 1
-            synthesizer.envelope.sustain -= 0.1
-          end
-          synthesizer.envelope.sustain = synthesizer.envelope.sustain.clamp(0.0, 1.0)
-          puts "Sustain: #{synthesizer.envelope.sustain.round(2)}"
-
+          new_value = value == 127 ? 0.01 : -0.01
+          groovebox.current_instrument.envelope.sustain = groovebox.current_instrument.envelope.sustain.clamp(0.00, 1.00)
+          puts "Sustain: #{groovebox.current_instrument.envelope.sustain.round(2)}"
         when 76 # Release
-          if value == 127
-            synthesizer.envelope.release += 1
-          elsif value == 1
-            synthesizer.envelope.release -= 1
-          end
-          synthesizer.envelope.release = synthesizer.envelope.release.clamp(0.01, 10.0)
-          puts "Release: #{synthesizer.envelope.release.round(2)}"
+          new_value = value == 127 ? 0.01 : -0.01
+          groovebox.current_instrument.envelope.release = groovebox.current_instrument.envelope.release.clamp(0.00, 10.00)
+          puts "Release: #{groovebox.current_instrument.envelope.release.round(2)}"
+        end
+
+        case control
+        when 40 # Channel Select
+          groovebox.change_channel(3)
+        when 41
+          groovebox.change_channel(2)
+        when 42
+          groovebox.change_channel(1)
+        when 43
+          groovebox.change_channel(0)
         end
 
         if control == config['start'] && value == 127
@@ -213,20 +213,62 @@ end
 
 FFI::PortAudio::API.Pa_Initialize
 
+class Groovebox
+  def initialize
+    @instruments = []
+    @current_channel = 0
+  end
+
+  def add_instrument(instrument)
+    @instruments.push instrument
+  end
+
+  def change_channel(channel)
+    @current_channel = channel
+  end
+
+  def current_instrument
+    @instruments[@current_channel]
+  end
+
+  def note_on(midi_note, velocity)
+    current_instrument.note_on(midi_note, velocity)
+  end
+
+  def note_off(midi_note)
+    current_instrument.note_off(midi_note)
+  end
+
+  def generate(frame_count)
+    current_instrument.generate(frame_count)
+  end
+end
+
 begin
   config = YAML.load_file('midi_config.yml')
+
+  groovebox = Groovebox.new
+
   synthesizer = Synthesizer.new(SAMPLE_RATE, AMPLITUDE)
-  stream = VCA.new(synthesizer, SAMPLE_RATE, BUFFER_SIZE)
+  groovebox.add_instrument synthesizer
+
+  kick = Presets::Kick.new
+  drum_rack = DrumRack.new(68)
+  drum_rack.add_track(kick, 0)
+
+  groovebox.add_instrument drum_rack
+
+  stream = VCA.new(groovebox, SAMPLE_RATE, BUFFER_SIZE)
 
   DRb.start_service
   sequencer = DRbObject.new_with_uri('druby://localhost:8787')
 
-  sequencer_player = SequencerPlayer.new(synthesizer, sequencer)
+  sequencer_player = SequencerPlayer.new(kick, sequencer)
 
   puts "Playing sound. Use MIDI to control:"
   puts "  MIDI: Note range #{config['keyboard']['note_range']['start']} - #{config['keyboard']['note_range']['end']}"
   Thread.new do
-    handle_midi_signals(synthesizer, sequencer_player, config)
+    handle_midi_signals(groovebox, sequencer_player, config)
   end
 
   DRb.thread.join
